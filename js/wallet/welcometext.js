b4798b2e39ee8662076b35773c62fac06138be4e
Марсель <63909335+marselshow@users.noreply.github.com>
2022-02-22 01:29:37 +0400

    yaEblan98

---
+++ js/wallet/welcometext.js
@@ -0,0 +1,20 @@
+const welcome_texts = [
+    "Welcome back!",
+    "Welcome!",
+    "Hey there!"
+];
+$("#welcometext").text(welcome_texts[Math.random() * welcome_texts.length | 0]);
+
+
+const backrounds = [
+    "img/1.jpg",
+    "https://game-guide.fr/wp-content/uploads/2020/06/AmongTreesScreenshot5.jpg",
+    "https://i.imgur.com/csou8q3.jpeg"
+];
+document.querySelector("#background").setAttribute("data-background", backrounds[Math.random() * backrounds.length | 0]);
+loadImages();
+
+function showcredits() {
+    $("html").addClass("is-clipped");
+    $("#modal").addClass("is-active");
+};
